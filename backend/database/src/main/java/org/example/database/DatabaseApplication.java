cb9be52a47af0c5369a37c5043c6be71f3193983
daizxn <daizxn@outlook.com>
2025-06-24 10:24:26 +0800

    feat(commin): 创建result模板

--- backend/database/src/main/java/org/example/database/DatabaseApplication.java
+++ backend/database/src/main/java/org/example/database/DatabaseApplication.java
@@ -1,9 +1,11 @@
 package org.example.database;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.example.database.mapper")
 public class DatabaseApplication {
 
     public static void main(String[] args) {
---
+++ backend/database/src/main/java/org/example/database/common/Result.java
@@ -0,0 +1,54 @@
+package org.example.database.common;
+
+
+import lombok.Data;
+import org.example.database.common.enums.ResultCodeEnum;
+
+@Data
+public class Result {
+    private String code;
+    private String msg;
+    private Object data;
+
+    private Result(Object data) {
+        this.data = data;
+    }
+
+    public Result() {
+    }
+
+    public static Result success() {
+        Result tResult = new Result();
+        tResult.setCode(ResultCodeEnum.SUCCESS.code);
+        tResult.setMsg(ResultCodeEnum.SUCCESS.msg);
+        return tResult;
+    }
+
+    public static Result success(Object data) {
+        Result tResult = new Result(data);
+        tResult.setCode(ResultCodeEnum.SUCCESS.code);
+        tResult.setMsg(ResultCodeEnum.SUCCESS.msg);
+        return tResult;
+    }
+
+    public static Result error() {
+        Result tResult = new Result();
+        tResult.setCode(ResultCodeEnum.SYSTEM_ERROR.code);
+        tResult.setMsg(ResultCodeEnum.SYSTEM_ERROR.msg);
+        return tResult;
+    }
+
+    public static Result error(String code, String msg) {
+        Result tResult = new Result();
+        tResult.setCode(code);
+        tResult.setMsg(msg);
+        return tResult;
+    }
+
+    public static Result error(ResultCodeEnum resultCodeEnum) {
+        Result tResult = new Result();
+        tResult.setCode(resultCodeEnum.code);
+        tResult.setMsg(resultCodeEnum.msg);
+        return tResult;
+    }
+}
