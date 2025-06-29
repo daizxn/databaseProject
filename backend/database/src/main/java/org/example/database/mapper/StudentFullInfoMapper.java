eb529d6d02ee6a04d9c8647058be75d4be0ca3fb
daizxn <daizxn@outlook.com>
2025-06-29 23:15:55 +0800

    feat(StudentFullInfo): add StudentFullInfo entity, mapper, service, and XML configuration for student management

---
+++ backend/database/src/main/java/org/example/database/mapper/StudentFullInfoMapper.java
@@ -0,0 +1,19 @@
+package org.example.database.mapper;
+
+
+import org.example.database.entity.StudentFullInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author daizxn
+* @description 针对表【view_student_full_info】的数据库操作Mapper
+* @createDate 2025-06-28 22:39:36
+* @Entity org.example.database.entity.StudentFullInfo
+*/
+public interface StudentFullInfoMapper extends BaseMapper<StudentFullInfo> {
+
+}
+
+
+
+
---
+++ backend/database/src/main/java/org/example/database/service/StudentFullInfoService.java
@@ -0,0 +1,13 @@
+package org.example.database.service;
+
+import org.example.database.entity.StudentFullInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author daizxn
+* @description 针对表【view_student_full_info】的数据库操作Service
+* @createDate 2025-06-28 22:39:36
+*/
+public interface StudentFullInfoService extends IService<StudentFullInfo> {
+
+}
